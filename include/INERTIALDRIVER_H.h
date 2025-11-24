306a8b481d0daa767c923e95cc5f9bba7c610777
Genzini Giovanni <giovanni.genzini@studenti.unipd.it>
2025-11-24 09:39:15 +0100

    Aggiunti commenti al codice

--- include/INERTIALDRIVER_H.h
+++ include/INERTIALDRIVER_H.h
@@ -19,15 +19,15 @@ class inertialDriver{
 
         inertialDriver(int dim=50);
 
-        void push_back(const Measure& m);
-        Measure pop_front();
-        void clear_buffer();
-        reading& get_reading(const int index);
-        int size() const { return buffer.size(); }
-        bool isEmpty() const { return buffer.size() == 0; }
+        void push_back(const Measure& m); //memorizza misura nel buffer(sovrascrive la meno recente se è pieno)
+        Measure pop_front(); //ritorna misura più vecchia e la rimuove
+        void clear_buffer(); //elimina tutte le misure
+        reading& get_reading(const int index); //ritorna la lettura della misura più recente corrispondente al numero passato
+        int size() const { return buffer.size(); } //ritorna numero di misure presenti nel buffer
+        bool isEmpty() const { return buffer.size() == 0; } //trona true se il buffer è vuoto
 };
 
-ostream& operator<<(ostream& out, const inertialDriver& driver);
+ostream& operator<<(ostream& out, const inertialDriver& driver); //overloading dell'operatore << per stampare ultima misura presente nel buffer
 
 #endif
 
